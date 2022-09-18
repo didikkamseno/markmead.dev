07582307705e9027e3d1f93f8be63be12275cd1f
didikkamseno <mybigidea28@gmail.com>
2022-09-19 02:54:47 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className="p-4 text-center border border-slate-100 bg-slate-50 rounded-xl dark:bg-slate-800 dark:border-slate-700">
       <div className="flex items-center justify-between gap-4">
         <p className="text-sm text-slate-500 dark:text-slate-400">
-          {new Date().getFullYear()} Mark Mead.
+          {new Date().getFullYear()} Didik Kamseno.
         </p>
 
         <select
@@ -15,8 +15,8 @@ export default function Footer() {
           className="text-sm rounded-lg bg-slate-50 border-slate-200 dark:bg-slate-800 dark:border-slate-700 text-slate-700 dark:text-slate-200"
         >
           <option>Latest</option>
-          <option value="https://v2.markmead.dev/">v2</option>
-          <option value="https://v1.markmead.dev/">v1</option>
+          <option value="https://didikkamseno.com/">v2</option>
+          <option value="https://didikkamseno.com/">v1</option>
         </select>
       </div>
     </footer>
